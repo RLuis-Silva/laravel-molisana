c7a031a1bee69b3c8beeb4eaa8013e074a267cfd
Luis <s.ros.lui@gmail.com>
2021-01-07 19:04:09 +0100

    q

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -5,9 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
+        {{-- link stile CSS --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         <title>Laravel</title>
     </head>
     <body>
-        
+        <header>
+            <a href="/" class="brand">LOGO sito</a>
+        </header>
+
+        <main>
+            <h1>Welcome al sito!</h1>
+        </main>
     </body>
 </html>
